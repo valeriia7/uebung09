5a905755b7a77e0763717e27f1cd802712e092cd
valeriia7 <Valeriia@DESKTOP-RLDM2D8>
2018-06-19 23:17:56 +0200

    init

--- src/main/java/ueb09/fz/Auto.java
+++ src/main/java/ueb09/fz/Auto.java
@@ -1,27 +1,28 @@
 package ueb09.fz;
 
 public class Auto extends Kraftfahrzeug {
-	private int zulGesamtGewicht;
+
 	private int sitze;
 	private int tueren;
 	private Scheibenbremse[] bremsen;
 
-	public Auto(String hersteller, String modell, int ps, int hubraum, int tankvolumen, int sitze, int tueren, int zulGesamtGewicht) {
+	public Auto(String hersteller, String modell, int ps, int hubraum, int tankvolumen, int sitze,int zulGesamtGewicht,int tueren  ) {
 		super(hersteller,modell, new Scheibenbremse[] {
 				new Scheibenbremse(),
 				new Scheibenbremse(),
 				new Scheibenbremse(),
-				new Scheibenbremse()},ps,hubraum,tankvolumen);
+				new Scheibenbremse()},ps,hubraum,tankvolumen,zulGesamtGewicht);
 		this.sitze = sitze;
-		this.zulGesamtGewicht = zulGesamtGewicht;
+		this.tueren = tueren;
+
 
 	}
 
 	public String toString() {
-		return super.toString()
+		return super.toString()+ "mit"
 				+ sitze + " Sitzen, "
 				+ tueren + " Türen, "
-				+ bremsen.length + " " + bremsen[0].toString()
-				+ " und " + zulGesamtGewicht + " zul. Gesamtgewicht";
+				+ bremsen.length + " " + bremsen[0].toString();
+
 	}
 }
